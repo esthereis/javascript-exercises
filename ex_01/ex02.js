396b6d5075e8ec5d70fa4f036f49a5212172f3ed
Esther Reis <estherjuliane_reis@hotmail.com>
2023-08-03 16:26:23 +0200

    ex01 completed

---
+++ ex_01/ex02.js
@@ -0,0 +1,7 @@
+// Calculate the area of a rectangle using variables for width and height. Print the result.
+
+function calculateArea(width, height) {
+  return width * height;
+}
+
+console.log(calculateArea(5, 3));
---
+++ ex_01/ex05.js
@@ -0,0 +1,6 @@
+// Create an array of your favorite foods and print the elements to the console.
+let food = ['brigadeiro', 'barbecue', 'pudim', 'feijoada', 'pastel'];
+
+for (let i = 0; i < food.length; i++) {
+  console.log(food[i]);
+}
